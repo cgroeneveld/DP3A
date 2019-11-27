bba9ad2683571a924723f07e3588253568572f72
Christian Groeneveld <groeneveld@student18.strw.leidenuniv.nl>
2019-11-27 10:04:51 +0100

    addded

--- reduction_steps/test.py
+++ reduction_steps/test.py
@@ -1,5 +1,5 @@
-import phase_cal as pc
+import diag_cal as pc
 
-gs = pc.PhaseCalibrator(0,'/data1/groeneveld/LB_295_single/SB430.avg.ms/', '/data1/groeneveld/autodeploy/LB1/testrun1/', '../')
+gs = pc.DiagonalCalibrator(3,'/data1/groeneveld/LB_295_single/SB430.avg.ms/', '/data1/groeneveld/autodeploy/LB1/testrun1/', '../')
 gs.initialize()
 gs.execute()
