ce0d6623fe942193743ba72951c4b0fc8fa3d7d9
Christian Groeneveld <groeneveld@strw.leidenuniv.nl>
2020-01-01 18:06:37 +0100

    now allows for importing run.py

--- run.py
+++ run.py
@@ -13,68 +13,81 @@ import reduction_steps.phase_up as pu
 import datetime
 import quality_check as qc
 
-parser = argparse.ArgumentParser(description='An automation script')
-parser.add_argument('-p', type = str, help = "Path to where we can write the images and solution plots", default = './RESULTS/')
-parser.add_argument('-ms', type = str, help = "Location of measurement set", required = True)
-parser.add_argument('-s', type = str, help = "String representing the reduction steps. Use h for more help", required = True)
-parser.add_argument('-d', action = 'store_true', help = 'Enables debug mode')
-parser.add_argument('-y', action = 'store_true', help = 'Automatically accept phase-up warning')
-parser.add_argument('-m', type = str, help = "Path to the location of a model FITS file, used whenever we need to predict the model", default = None)
+class FakeParser(obj):
+    def __init__(self, ms, p, s, d, y, m):
+        self.ms = ms
+        self.p = p
+        self.s = s
+        self.d = d
+        self.y = y
+        self.m = m
 
-parsed = parser.parse_args()
+def main(parsed):
+    if parsed.s == 'h':
+        print('''
+            The following reduction steps have (so far) been implemented:
+            p  |   Phase only calibration
+            d  |   Diagonal calibration, meaning one phase and one diagonal calibration
+            t  |   TEC calibration for the ionosphere, basically a constraint on the
+            |   phase-only calibration
+            u  |   Phase-up: mash all short baselines together to one single base station
+            |   Requires a model.
+            _____________________________________________________________________________
 
-if parsed.s == 'h':
-    print('''
-        The following reduction steps have (so far) been implemented:
-        p  |   Phase only calibration
-        d  |   Diagonal calibration, meaning one phase and one diagonal calibration
-        t  |   TEC calibration for the ionosphere, basically a constraint on the
-           |   phase-only calibration
-        u  |   Phase-up: mash all short baselines together to one single base station
-           |   Requires a model.
-        _____________________________________________________________________________
+            These have not yet been implemented:
+            r  |   Correction for rotation measure. Important for low frequencies
+            m  |   Predict using a new model - requires a model
+        ''')
 
-        These have not yet been implemented:
-        r  |   Correction for rotation measure. Important for low frequencies
-        m  |   Predict using a new model - requires a model
-    ''')
+    redsteps = list(parsed.s)
+    uni_redsteps = np.unique(redsteps)
+    nlist = np.zeros(len(redsteps))
+    for chara in uni_redsteps: 
+        mask = chara == np.asarray(redsteps)
+        nlist[mask] = np.arange(1, int(1+sum(mask)))
 
-redsteps = list(parsed.s)
-uni_redsteps = np.unique(redsteps)
-nlist = np.zeros(len(redsteps))
-for chara in uni_redsteps: 
-    mask = chara == np.asarray(redsteps)
-    nlist[mask] = np.arange(1, int(1+sum(mask)))
+    if 'u' in redsteps and not parsed.y:
+        print("This reduction strings contains a phase-up. Phase-ups are destructive - so please make sure that you have backed your system up. Type 'ok' to continue: ")
+        ans = raw_input()
+        if ans != 'ok':
+            sys.exit()
 
-if 'u' in redsteps and not parsed.y:
-    print("This reduction strings contains a phase-up. Phase-ups are destructive - so please make sure that you have backed your system up. Type 'ok' to continue: ")
-    ans = raw_input()
-    if ans != 'ok':
-        sys.exit()
+    if 'u' in redsteps or 'm' in redsteps:
+        assert parsed.m != None
 
-if 'u' in redsteps or 'm' in redsteps:
-    assert parsed.m != None
+    for red, n in zip(redsteps, nlist):
+        n = int(n)
+        if red == 'p':
+            cal = pc.PhaseCalibrator(n, parsed.ms, parsed.p, './parsets/')
+        elif red == 'd':
+            cal = dc.DiagonalCalibrator(n, parsed.ms, parsed.p, './parsets/')
+        elif red == 't':
+            cal = tc.TecCalibrator(n, parsed.ms, parsed.p, './parsets/')
+        elif red == 'u':
+            cal = pu.PhaseUp(n, parsed.ms, parsed.p, './parsets/', parsed.m)
+        else:
+            print("Reduction step {} not implemented".format(red))
+        if parsed.d:
+            cal.DEBUG = True
+        cal.initialize()
+        cal.execute()
 
-for red, n in zip(redsteps, nlist):
-    n = int(n)
-    if red == 'p':
-        cal = pc.PhaseCalibrator(n, parsed.ms, parsed.p, './parsets/')
-    elif red == 'd':
-        cal = dc.DiagonalCalibrator(n, parsed.ms, parsed.p, './parsets/')
-    elif red == 't':
-        cal = tc.TecCalibrator(n, parsed.ms, parsed.p, './parsets/')
-    elif red == 'u':
-        cal = pu.PhaseUp(n, parsed.ms, parsed.p, './parsets/', parsed.m)
-    else:
-        print("Reduction step {} not implemented".format(red))
-    if parsed.d:
-        cal.DEBUG = True
-    cal.initialize()
-    cal.execute()
+    qc.main(parsed.p, redsteps, nlist)
 
-qc.main(parsed.p, redsteps, nlist)
+    log = jp.Locker(parsed.p + 'log')
+    log['ms'] = parsed.ms
+    log['last_edit'] = datetime.datetime.now().strftime("%Y_%m_%d_%H_$M")
+    log.save()
 
-log = jp.Locker(parsed.p + 'log')
-log['ms'] = parsed.ms
-log['last_edit'] = datetime.datetime.now().strftime("%Y_%m_%d_%H_$M")
-log.save()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='An automation script')
+    parser.add_argument('-p', type = str, help = "Path to where we can write the images and solution plots", default = './RESULTS/')
+    parser.add_argument('-ms', type = str, help = "Location of measurement set", required = True)
+    parser.add_argument('-s', type = str, help = "String representing the reduction steps. Use h for more help", required = True)
+    parser.add_argument('-d', action = 'store_true', help = 'Enables debug mode')
+    parser.add_argument('-y', action = 'store_true', help = 'Automatically accept phase-up warning')
+    parser.add_argument('-m', type = str, help = "Path to the location of a model FITS file, used whenever we need to predict the model", default = None)
+
+    parsed = parser.parse_args()
+    main(parsed)
