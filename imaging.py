270bff1560d4cf685fe23e4519777fac33d6b79c
Christian Groeneveld <groeneveld@strw.leidenuniv.nl>
2020-09-26 18:52:17 +0200

    now you can draw regions

--- imaging.py
+++ imaging.py
@@ -10,6 +10,63 @@ import os
 import sys
 from scipy.ndimage import zoom
 
+class SelectFromCollection(object):
+    """Select indices from a matplotlib collection using `LassoSelector`.
+
+    Selected indices are saved in the `ind` attribute. This tool fades out the
+    points that are not part of the selection (i.e., reduces their alpha
+    values). If your collection has alpha < 1, this tool will permanently
+    alter the alpha values.
+
+    Note that this tool selects collection objects based on their *origins*
+    (i.e., `offsets`).
+
+    Parameters
+    ----------
+    ax : :class:`~matplotlib.axes.Axes`
+        Axes to interact with.
+
+    collection : :class:`matplotlib.collections.Collection` subclass
+        Collection you want to select from.
+
+    alpha_other : 0 <= float <= 1
+        To highlight a selection, this tool sets all selected points to an
+        alpha value of 1 and non-selected points to `alpha_other`.
+    """
+
+    def __init__(self, ax, collection, alpha_other=0.3):
+        self.canvas = ax.figure.canvas
+        self.collection = collection
+        self.alpha_other = alpha_other
+
+        self.xys = collection.get_offsets()
+        self.Npts = len(self.xys)
+
+        # Ensure that we have separate colors for each object
+        self.fc = collection.get_facecolors()
+        if len(self.fc) == 0:
+            raise ValueError('Collection must have a facecolor')
+        elif len(self.fc) == 1:
+            self.fc = np.tile(self.fc, (self.Npts, 1))
+
+        self.lasso = LassoSelector(ax, onselect=self.onselect)
+        self.ind = []
+
+    def onselect(self, verts):
+        path = Path(verts)
+        self.ind = np.nonzero(path.contains_points(self.xys))[0]
+        self.fc[:, -1] = self.alpha_other
+        self.fc[self.ind, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+
+    def disconnect(self):
+        self.lasso.disconnect_events()
+        self.fc[:, -1] = 1
+        self.collection.set_facecolors(self.fc)
+        self.canvas.draw_idle()
+
+
 def _compute_beam(head):
     beam_area = np.pi/(4*np.log(2)) * head['BMAJ'] * head['BMIN']
     area_pixels = beam_area / head['CDELT2']**2
@@ -148,10 +205,27 @@ def plot_MFS(path_to_resolved, opts={}):
     coord = WCS(head) # And generate WCS headers for the axes
     mpl.rcParams.update({'axes.labelsize': 16, 'xtick.labelsize':14})
 
+    xpoints,ypoints = np.mgrid[:512,:512]
+
     f = plt.figure()
     ax = plt.subplot(projection=coord, slices=('x','y',0,0))
+    collection = ax.scatter(xpoints,ypoints,alpha=0)
     ax.imshow(data, origin='lower',vmin=0)
+
+    sels = []
+    selector = SelectFromCollection(ax,collection)
+    def accept(event):
+        if event.key == 'enter':
+            sels.append(selector.xys[selector.ids])
+            ax.set_title('Press enter to accept selected points as a region')
+            f.canvas.draw()
+    
+    f.canvas.mpl_connect("key_press_event",accept)
+    ax.set_title("Press enter to accept selected points as a region")
+
     plt.show()
+
+    print(sels)
         
 
 
